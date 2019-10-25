80779dab479b2a2a675703f7c33c47d9500f6e85
admin@163.com <admin@163.com>
2019-10-25 15:27:35 +0800

    Initial commit

---
+++ yunpian.py
@@ -0,0 +1,28 @@
+# coding: utf-8
+# 云片网短信平台
+import requests
+
+APIKEY = ''
+
+def send_message(code, phone):
+    # code 验证码，phone 手机号
+    url = 'https://sms.yunpian.com/v2/sms/single_send.json'
+    text = '您的验证码是{}'.format(code)
+    headers = {
+        'Accept': 'application/json;charset=utf-8;',
+        'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+    }
+    data = {
+        'apikey': APIKEY,
+        'mobile': phone,
+        'text': text
+    }
+    res = requests.post(url, data=data, headers=headers)
+    res.encoding = 'utf-8'
+    content = res.json()
+    print(content)
+
+
+if __name__ == '__main__':
+    send_message('123456', '')
+
